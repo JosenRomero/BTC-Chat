450d72b863287d576d9ed9b9fa4c3943960690e8
Josen Romero <romerojosemanuel@hotmail.com>
2023-04-30 20:36:49 -0600

    update translations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
+import { useTranslation } from 'react-i18next'
 
 const Footer = () => {
+  const { t } = useTranslation()
+
   return (
     <footer className="flex container justify-end mx-auto pt-5 pb-2">
-      <p className="text-zinc-800">Made by <a className="text-zinc-800 hover:text-zinc-800 hover:underline" href="https://github.com/JosenRomero" target="_blank" rel="noreferrer">José Romero</a></p>
+      <p className="text-zinc-800">{ t('footerText') } <a className="text-zinc-800 hover:text-zinc-800 hover:underline" href="https://github.com/JosenRomero" target="_blank" rel="noreferrer">José Romero</a></p>
     </footer>
   )
 }
--- src/components/QuestionInput.tsx
+++ src/components/QuestionInput.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
   addQuestion: (question: string) => void
@@ -6,6 +7,7 @@ interface Props {
 
 const QuestionInput = ({ addQuestion }: Props) => {
   const [question, setQuestion] = useState('')
+  const { t } = useTranslation()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -24,9 +26,9 @@ const QuestionInput = ({ addQuestion }: Props) => {
           placeholder=" "
           required
         />
-        <label className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Question</label>
+        <label className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{ t('addAQuestion') }</label>
       </div>
-      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Send</button>
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{ t('send') }</button>
     </form>
   )
 }
